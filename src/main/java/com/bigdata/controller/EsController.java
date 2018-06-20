037460db87edc5f8596db0cbdb6ba7326c28eaf4
LICHENG <291215654@qq.com>
2018-06-20 23:02:54 +0800

    json

--- src/main/java/com/bigdata/controller/EsController.java
+++ src/main/java/com/bigdata/controller/EsController.java
@@ -8,9 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -32,6 +30,16 @@ public class EsController {
     @RequestMapping("welcome.do")
     public String welcome() throws Exception {
         return "./welcome.jsp";
+    }
+
+    @RequestMapping("json.do")
+    @ResponseBody
+//    public HashMap<String,String> json(@RequestBody  Person person) throws Exception {
+    public HashMap<String,String> json() throws Exception {
+        HashMap<String,String> map = new HashMap<String,String>();
+        map.put("aaa","aaa");
+        map.put("bbb","bbb");
+        return map;
     }
 
     @RequestMapping("search.do")
---
+++ src/main/java/com/bigdata/controller/Person.java
@@ -0,0 +1,30 @@
+package com.bigdata.controller;
+
+/**
+ * Created by LICHENG on 2018/6/20.
+ */
+public class Person {
+
+    private String name;
+    private String age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+
+
+}
